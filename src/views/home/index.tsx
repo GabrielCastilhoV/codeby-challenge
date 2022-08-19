4a43e8efd37251ff9cdfb9df757c45cfb2cbd2c3
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-08-19 09:01:57 -0400

    feat: create home view

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,33 @@
-import { Button } from 'components/elements'
+import { useState } from 'react'
+
+import { Button, Cart } from 'components/elements'
+
+import above from 'data/above.json'
+import below from 'data/below.json'
+
 import * as S from './styles'
 
 export const HomeView = () => {
+  const [showCartId, setShowCartId] = useState<number>(0)
+
   return (
     <S.Wrapper>
-      <Button active>Carrinho 1</Button>
-      <Button>Carrinho 2</Button>
+      <S.ButtonsContainer>
+        <Button active={showCartId === 0} onClick={() => setShowCartId(0)}>
+          Carrinho 1
+        </Button>
+
+        <Button active={showCartId === 1} onClick={() => setShowCartId(1)}>
+          Carrinho 2
+        </Button>
+      </S.ButtonsContainer>
+
+      <S.CartContainer>
+        <Cart
+          products={showCartId === 0 ? above.items : below.items}
+          totalizers={showCartId === 0 ? above.totalizers : below.totalizers}
+        />
+      </S.CartContainer>
     </S.Wrapper>
   )
 }
--- src/views/home/styles.ts
+++ src/views/home/styles.ts
@@ -3,5 +3,24 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   width: 100%;
   min-height: 100vh;
+  padding: 10px;
   background: ${({ theme }) => theme.colors.grey300};
+`
+
+export const ButtonsContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 20%;
+  transform: translateY(-50%);
+
+  display: grid;
+  gap: 16px;
+`
+
+export const CartContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
